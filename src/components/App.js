0c095f002e0ce6fd74ad801e2100c58e3c73d0cb
konalam venkatesh <konalamvenkatesh111@gmail.com>
2023-02-20 11:12:48 +0530

    add para

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [para, setPara] = useState("");
+  const addpara = () => {
+    setPara(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <p id="para">{para}</p>
+      <button id="click" onClick={addpara}>
+        click here
+      </button>
     </div>
   );
 }
-
 
 export default App;
