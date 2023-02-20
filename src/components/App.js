a07e7d9c39244f641d25493b2cc58d2b6f338d13
konalam venkatesh <konalamvenkatesh111@gmail.com>
2023-02-20 14:10:12 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,19 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  let [para, setPara] = useState("");
+  let [isClicked, setIsClicked] = useState(false);
   const addpara = () => {
-    setPara(
-      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-    );
+    setIsClicked(true);
   };
   return (
     <div id="main">
       {/* // Do not alter the main div */}
-      <p id="para">{para}</p>
+      {isClicked && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
       <button id="click" onClick={addpara}>
         click here
       </button>
